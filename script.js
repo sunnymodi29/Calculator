44cb5a91025d9367dddd33c467b4b15c5617f04f
Sunny Modi <56613441+sunnymodi29@users.noreply.github.com>
2022-05-01 16:45:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let outputScreen = document.getElementById("output-screen");
+
+function display(num) {
+    outputScreen.value += num;
+}
+function Calculate() {
+    try {
+        outputScreen.value = eval(outputScreen.value);
+    }
+    catch(err)
+    {
+        alert("Invalid");
+    }
+}
+function Clear() {
+    outputScreen.value = "";
+}
